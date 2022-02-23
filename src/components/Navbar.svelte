a170f6e816989b0494eb38f566b250eea4382c1e
joevtap <joelvitortorres@gmail.com>
2022-02-23 18:56:09 -0300

    Fixed spacing

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -4,14 +4,11 @@
 
 <nav class="bg-white/25 fixed z-10 w-full flex items-center justify-between px-16 h-16">
   <img class="w-12 object-contain" {src} alt="League of Byron">
-
-
   <ul class="flex flex-wrap items-center">
     <li class="text-slate-700 hover:text-slate-500 py-2 mr-14"><a href=".">Mostre sua torcida</a></li>
     <li class="text-slate-700 hover:text-slate-500 py-2 mr-14"><a href=".">Times</a></li>
     <li class="text-slate-700 hover:text-slate-500 py-2 "><a href=".">Redes sociais</a></li>
   </ul>
-
 </nav>
 
 <style>
